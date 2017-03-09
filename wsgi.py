194d7a60f3a7186fa291fca32c1a72af48bc6c01
HG18 <huaguan0412@yahoo.com>
2017-03-08 22:33:39 -0500

    string

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World from HG!"
+    str1 = "Hello World from HG!"
+    return str1
 
 if __name__ == "__main__":
     application.run()
