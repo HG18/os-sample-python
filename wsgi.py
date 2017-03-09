356470349dc484c882a18255a5703cba3cea76c3
HG18 <huaguan0412@yahoo.com>
2017-03-09 01:22:23 -0500

    string

--- wsgi.py
+++ wsgi.py
@@ -13,38 +13,39 @@ def exe_cmd(c):
 	return o, e, p_s
 
 def pull_data():
-	cmd="curl -H \"Authorization: Bearer 8UoEBUYYqpfizZp-AnJtCFsCtAxSIUO9_xwFfF9A1as\" \"https://www.robotevents.com/robot-competitions/vex-robotics-competition/RE-VRC-17-5082.html\""
+# 	m = "curl -H \"Authorization: Bearer 8UoEBUYYqpfizZp-AnJtCFsCtAxSIUO9_xwFfF9A1as\" \"https://www.robotevents.com/robot-competitions/vex-robotics-competition/RE-VRC-17-5082.html\""
 
-	(output,err,p_status)=exe_cmd(cmd)
-	if p_status == 0:
+#j	(output,err,p_status)=exe_cmd(m)
+#o	if p_status == 0:
+    output = subprocess.check_output(['curl','-H','Authorization: Bearer 8UoEBUYYqpfizZp-AnJtCFsCtAxSIUO9_xwFfF9A1as','https://www.robotevents.com/robot-competitions/vex-robotics-competition/RE-VRC-17-5082.html'])
 		#print output
-		flag = 0
-		buf = output.split("\n")
+    flag = 0
+    buf = output.split("\n")
 		
-		for line in buf:
-			if not (line.find('<results') == -1):
-				flag = 1
-			if (flag == 1 ) & (not (line.find('data=') == -1)):
-				flag = 0
+    l_d = []
+    for line in buf:
+	if not (line.find('<results') == -1):
+            flag = 1
+            if (flag == 1 ) & (not (line.find('data=') == -1)):
+		flag = 0
 				# found the result data block
-				dline= line.split("}")
+		dline= line.split("}")
 
-				l_d = []
-				for dlin in dline:
-					# dl hs one row of the result
-					dli = dlin.split("{")
-					if ( len(dli) > 1):
-						dl = dli[1].replace("&quot;","")
+		for dlin in dline:
+				# dl hs one row of the result
+                    dli = dlin.split("{")
+                    if ( len(dli) > 1):
+			dl = dli[1].replace("&quot;","")
 						#print dl
 						# 
-						elem = dl.split(",")
+			elem = dl.split(",")
 						
-						d = {}
-						for ele in elem:
-							el=ele.split(":")
-							d[el[0]]=el[1]
-						l_d.append(d)
-	return l_d
+			d = {}
+			for ele in elem:
+				el=ele.split(":")
+				d[el[0]]=el[1]
+			l_d.append(d)
+    return l_d
 
 
 #############################################
